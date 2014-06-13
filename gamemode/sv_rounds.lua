7c06a6fbc2343fa828c5adcaa20f9ad2b5bdaff9
Benton Liu <liubenton@yahoo.com>
2014-06-13 00:32:43 -0700

    Update sv_rounds.lua

--- gamemode/sv_rounds.lua
+++ gamemode/sv_rounds.lua
@@ -271,15 +271,15 @@ function GM:StartNewRound()
 		ply:UnSpectate()
 	end
 	for _, ent in pairs(ents.FindByClass("prop_ragdoll")) do
- -	    if IsValid(ent) then
- -	        ent:SetNoDraw(true)
- -	        ent:SetSolid(SOLID_NONE)
- -	        ent:SetColor(Color(0,0,0,0))
- -			ent:SetNWEntity("Player",self)
- -			ent:SetNWString("Name", "")
- -	        ent.NoTarget = true
- -	    end
- -	end
+ 	    if IsValid(ent) then
+ 	        ent:SetNoDraw(true)
+ 	        ent:SetSolid(SOLID_NONE)
+ 	        ent:SetColor(Color(0,0,0,0))
+ 			ent:SetNWEntity("Player",self)
+ 			ent:SetNWString("Name", "")
+ 	        ent.NoTarget = true
+ 	    end
+ 	end
 	game.CleanUpMap()
 	self:InitPostEntityAndMapCleanup()
 	self:ClearAllFootsteps()
